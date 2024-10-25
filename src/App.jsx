5e6a4e73095548385c5254a79a576d206661ad5d
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-10-24 23:29:18 -0400

    added details route with params

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import DashBoard from "./pages/DashBoard";
+import HeroDetails from "./pages/HeroDetails";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<DashBoard/>}/>
-    
+      <Route path="/details:id" element={<HeroDetails/>}/>
     </Routes>
     
     </BrowserRouter>
